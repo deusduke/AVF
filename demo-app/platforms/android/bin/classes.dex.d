4db7824360d007235239095f6803303b941fe989
Deus Duke <deus@deusenterprises.com>
2013-08-09 21:26:19 -0500

    added android version

---
+++ demo-app/platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes.dex : \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/AVFDemoApp.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/BuildConfig.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R$attr.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R$drawable.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R$layout.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R$string.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R$xml.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/classes/com/fullsail/testApp/R.class \
+/Users/deusduke/Dropbox/FullSail/AVF/avf-apps/demo-app/platforms/android/bin/dexedLibs/cordova-3.0.0-684d83e30f238cea54fdb1099d5bccf8.jar \
