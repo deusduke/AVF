1fb3bfe8789bcbc7877203822cea3f3d03326795
Deus Duke <deus@deusenterprises.com>
2013-08-24 00:18:16 -0500

    final commit for week 03

---
+++ demo-app/platforms/android/assets/www/js/index.js
@@ -0,0 +1,47 @@
+/*
+ * Deus Duke
+ * AVF 1308
+ * Week 03
+ *
+ */
+var destinationType;
+var pictureSource;
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        window.onerror = function(message, url, lineNumber) {
+            console.log("Error: "+message+" in "+url+" at line "+lineNumber);
+        }
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        destinationType: navigator.camera.DestinationType;
+        pictureSource: navigator.camera.PictureSourceType;
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
